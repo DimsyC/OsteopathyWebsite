a8d5ad0f954a26d4d7ad8b668c7940c1f1c47a4d
DimsyC <dimsy_donoso@hotmail.com>
2019-11-10 21:33:13 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//Components
+import Navbar from "./NavBar"
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;

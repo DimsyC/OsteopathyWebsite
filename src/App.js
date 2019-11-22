f698af1bc8ee8ae643ba00ec6d6cc484a4d196f3
DimsyC <dimsy_donoso@hotmail.com>
2019-11-22 18:52:48 -0500

    Added Switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 //Components
-import Navbar from "./NavBar"
+import Navbar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+      <Navbar />
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact-us">
+              <ContactUs />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function ContactUs() {
+  return <h2>Contact Us</h2>;
 }
 
 export default App;
--- src/NavBar/Navbar.jsx
+++ src/NavBar/Navbar.jsx
@@ -19,9 +19,9 @@ function NavigationBar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Page1</Nav.Link>
-          <Nav.Link href="#link">Page2</Nav.Link>
-          <Nav.Link href="#link">Page3</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact-us">Contact us</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>

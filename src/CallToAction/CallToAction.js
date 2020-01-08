850106f6f527c58c1eea5216847fea80aa25394c
DimsyC <dimsy_donoso@hotmail.com>
2020-01-08 18:47:13 -0500

    added call to acton button

--- src/CallToAction/CallToAction.js
+++ src/CallToAction/CallToAction.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import GoRendezVous from "../GoRendezVous";
+import "./CallToAction.scss"
 
 function CallToAction(props) {
   const background = "/Images/ctaImage.jpg";
@@ -12,25 +13,29 @@ function CallToAction(props) {
         backgroundPosition: "center",
         backgroundRepeat: " no-repeat",
         backgroundSize: "cover",
-        padding:'10.5% 20%',
+        padding: "10.5% 20%"
       }}
     >
       <div
         style={{
           color: "white",
           textAlign: "center",
-          backgroundColor:'rgba(40, 154, 198, 0.35)',
-          paddingBottom:'2.5px'
-
+          backgroundColor: "rgba(40, 154, 198, 0.35)",
+          paddingBottom: "2.5px"
         }}
       >
         <h1
           style={{
-            fontSize: '4.5rem',
+            fontSize: "4.5rem",
             textTransform: "uppercase"
           }}
-        >Santé & Bien-Être</h1>
+        >
+          Santé & Bien-Être
+        </h1>
         <p>On vous Supporte. On vous Conseil. On vous Soigne</p>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center", marginTop:'15%'}}>
+        <button className='rendezVousBtn'>Prendre un Rendez-vous</button>
       </div>
     </div>
   );
